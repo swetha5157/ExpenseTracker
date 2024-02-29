515c0ca7ec2eca5ea4878e7d30a7fd7b83a369e9
Swetha <swetha.s2022cce@sece.ac.in>
2024-02-29 21:27:26 +0530

    form

--- static/index.js
+++ static/index.js
@@ -1,1 +1,40 @@
 console.log("from client side")
+
+// Create a form in static/index.html with 4 input fields
+
+// name: text
+// description: text area
+// Date: date
+// Amount: number
+
+// Submit button.
+
+
+// var f = document.createElement("FORM");
+// document.querySelector("body").append(f);
+
+// //name
+// var x= document.createElement("INPUT");
+// x.setAttribute("type", "text");
+// document.querySelector("body").append(x);
+
+// //description
+// var y = document.createElement("TEXTAREA");
+// document.querySelector("body").append(y);
+
+
+
+
+
+
+
+
+// //button
+// let newBtn=document.createElement("button");
+// newBtn.innerText="Calculate expense";
+// newBtn.classList.add("btn");
+// newBtn.style.display="flex";
+// newBtn.style.color="gray";
+// newBtn.style.fontStyle="bold";
+// console.log(newBtn);
+// document.querySelector("body").append(newBtn);
