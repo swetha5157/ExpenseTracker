cb08669617a3abc6ea3defb636561260b878ac41
Swetha <swetha.s2022cce@sece.ac.in>
2024-03-01 12:24:51 +0530

    post api

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+
+const bodyparser=require('body-parser')
 const app = express()
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}));
 
 const static = express.static('static');
 app.use("/",static);
@@ -8,13 +12,17 @@ app.use("/",static);
 **/
 
 
-app.get("/hi",(req,res)=>{
+app.post("/hi",(req,res)=>{
   console.log(req.query)
   res.json({
-    name:req.query.name,
-
-    phone:"123",
-    ph:req.query.ph*10,
+   // name:req.query.name,
+    name:req.body.name,
+    desc:req.body.desc,
+    date:req.body.date,
+    //date:req.body.date,
+    amount:req.body.amount
+   // phone:"123",
+    //ph:req.query.ph*10,
   });
 })
 app.listen(3000,() =>{
