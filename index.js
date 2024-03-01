d6ed72d163ac4e843d645a75d045a61325a2e596
Swetha <swetha.s2022cce@sece.ac.in>
2024-03-01 10:47:15 +0530

    req&res

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const app = express()
 
 const static = express.static('static');
 app.use("/",static);
+/*
+?key=value
+**/
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World...')
 
-// })
+app.get("/hi",(req,res)=>{
+  console.log(req.query)
+  res.json({
+    name:req.query.name,
 
+    phone:"123",
+    ph:req.query.ph*10,
+  });
+})
 app.listen(3000,() =>{
   console.log("Hello world")
 })
